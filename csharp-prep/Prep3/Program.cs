e04352ed429adc58090eb3cfc60e058de16a6282
cvans123 <van22047@byui.edu>
2024-09-24 05:51:23 +0200

    prep3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // User input number below to guess magic number.
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess !=magicNumber)
+
+        {
+            Console.Write("Guess a number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You found the magic number! Well done.");
+            }
+            
+        }
     }
 }
