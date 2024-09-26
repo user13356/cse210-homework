7d38ef0bcbb020a25ed4e3531be08da470c023ed
cvans123 <van22047@byui.edu>
2024-09-26 22:14:37 +0200

    Learning 02 Resume

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        job job1 = new job();
+        job1._jobtitle = "Backend Platform Developer";
+        job1._company = "Veritas";
+        job1._startYear = 2016;
+        job1.endYear = 2020;
+
+        job job2 = new job();
+        job2._jobtitle = "Senior Platform Engineer";
+        job2._company = "BBC"
+        job2._startYear = 2020;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Bella Newcastle";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+    
     }
-}
+    
+}        
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Backend Platform Developer";
+        job1._company = "Veritas";
+        job1._startYear = 2016;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Senior Platform Engineer";
+        job2._company = "BBC";
+        job2._startYear = 2020;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Bella Newcastle";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+    
     }
-}
+    
+}        
+
